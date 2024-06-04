44b7e5a559775202088ea9f4049e7b566f4044a9
mimilabomb <bartoszsz13@gmail.com>
2024-06-04 18:58:07 +0200

    end first task

---
+++ 01/app.js
@@ -0,0 +1,6 @@
+const user = ['Adam Nowak', 'Ala Kot', 'Tomek Iksiński', 'Wiktor Nowak', 'Ola Kowalska']
+
+console.log(user[0], '- Pierwszy użutkownik');
+console.log(user[2], '- Trzeci użutkownik');
+console.log(user[4], '- Piąty użutkownik');
+console.log(user.length);
